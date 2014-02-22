ae2a12b8dfcd5c65409063032060b8c31c156861
Kaia Konsap <kaia.konsap@gmail.com>
2014-02-22 17:34:18 +0200

    uus slider, mis vajab kohendamist veel

--- js/main.js
+++ js/main.js
@@ -5,4 +5,6 @@ $(document).ready(function(){
         infiniteLoop: false,
         hideControlOnEnd: true,
         pager: false
-    });})
+    });
+
+})
---
+++ js/owl.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    var owl = $("#owl1");
+    var owl2 = $("#owl2");
+
+    owl.owlCarousel({
+        pagination : false,
+        items : 4, //10 items above 1000px browser width
+        itemsDesktop : [1000,4], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile :[479,1] // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.prev');
+    });
+    owl2.owlCarousel({
+        pagination : false,
+        items : 3, //10 items above 1000px browser width
+        itemsDesktop : [1000,2], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsTablet: [600,1], //2 items between 600 and 0
+        itemsMobile :[479,1] // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl2.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl2.trigger('owl.prev');
+    })
+
+})
