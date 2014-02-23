8d7e6bde71eb9a434e29cc64626f086b29187d95
Kaia Konsap <kaia.konsap@gmail.com>
2014-02-23 23:27:35 +0200

    Majutusvaade, mis vajab isegi kujunduse osas järelemõtlemist

--- js/owl.js
+++ js/owl.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     var owl = $("#owl1");
     var owl2 = $("#owl2");
+    var owl3 = $("#owl3");
 
     owl.owlCarousel({
         pagination : false,
-        items : 5, //10 items above 1000px browser width
+        items : 4, //10 items above 1000px browser width
         itemsDesktop : [1000,4], //5 items between 1000px and 901px
         itemsDesktopSmall : [900,3], // betweem 900px and 601px
         itemsTablet: [600,2], //2 items between 600 and 0
@@ -33,6 +34,22 @@ $(document).ready(function(){
     })
     $(".prev").click(function(){
         owl2.trigger('owl.prev');
+    });
+    owl3.owlCarousel({
+        pagination : false,
+        items : 4, //10 items above 1000px browser width
+        itemsDesktop : [1150,3], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsTablet: [600,1], //2 items between 600 and 0
+        itemsMobile :[479,1] // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl3.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl3.trigger('owl.prev');
     })
 
 })
