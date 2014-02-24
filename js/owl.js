be209458239b2ed7ed6521a327f39673dcf329bb
Kaia Konsap <kaia.konsap@gmail.com>
2014-02-24 15:19:06 +0200

    Kontakti vaade, esialgne

--- js/owl.js
+++ js/owl.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     var owl = $("#owl1");
     var owl2 = $("#owl2");
     var owl3 = $("#owl3");
+    var owl4 = $("#owl4");
 
     owl.owlCarousel({
         pagination : false,
@@ -50,6 +51,22 @@ $(document).ready(function(){
     })
     $(".prev").click(function(){
         owl3.trigger('owl.prev');
+    });
+    owl4.owlCarousel({
+        pagination : false,
+        items : 2, //10 items above 1000px browser width
+        itemsDesktop : [1150,2], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,1], // betweem 900px and 601px
+        itemsTablet: [600,1], //2 items between 600 and 0
+        itemsMobile :[479,1] // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl4.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl4.trigger('owl.prev');
     })
 
 })
